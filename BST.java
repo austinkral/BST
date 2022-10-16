af08133aeb4a19b3f8aac0383c84846511681805
Austin Kral <austinckral@gmail.com>
2022-10-15 22:12:14 -0400

    added BST method signatures

--- BST.java
+++ BST.java
@@ -61,4 +61,55 @@ public class BST {
 	
     } // Node
 
+    private Node root;
+    private int size;
+
+    public BST() {
+	this.root = null;
+	this.size = 0;
+    } // BST
+
+    public int size() {
+	return this.size;
+    } // size
+
+    public void insert(int element) {
+	boolean isInserted = false;
+	while (!isInserted) {
+	    if (element > this.root.key) {} // do shit to the right
+	    if (element < this.root.key) {} // do shit to the left
+	} // while
+    } // insert
+
+    public void delete(int element) {
+	while (root.key != element) {} // do shit
+    } // delete
+
+    public void preorder() {
+	Node current = this.root;
+	this.preorderHelper(current);
+    } // preorder
+
+    private void preorderHelper(Node current) {
+	// recursively preorder the BST
+    } // preorderHelper
+    
+    public void postorder() {
+	Node current = this.root;
+	this.postorderHelper(current);
+    } // postorder
+
+    private void postorderHelper(Node current) {
+	// recursively postorder the BST
+    } // postorderHelper
+    
+    public void inorder() {
+	Node current = this.root;
+	this.inorderHelper(current);
+    } // inorder
+
+    private void inorderHelper(Node current) {
+	// recursively inorder the BST
+    } // inorderHelper
+    
 } // BST
